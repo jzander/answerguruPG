220a421619596a3ba056cbebff129e0a76208d2c
jzander <jeremy.zander@gmail.com>
2014-08-22 21:46:14 -0700

    updated stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823005807) do
+ActiveRecord::Schema.define(version: 20140823034450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "answers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "criterions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "decisions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "streets", force: true do |t|
     t.string   "name"
     t.string   "treetype"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
