576cc15da69cdf0996203576b807e672a8d29c9f
jzander <jeremy.zander@gmail.com>
2014-08-22 20:36:37 -0700

    answerguru on postgres

---
+++ config/routes.rb
@@ -0,0 +1,28 @@
+AnswerguruPg::Application.routes.draw do
+
+root to: 'decisions#new'
+
+  resources :decisions, only: [:index, :new, :create, :destroy, :edit] do
+    resources :criteria, only: [:index, :new, :create, :destroy]
+      resources :answers, only: [:index, :new, :create, :update, :destroy]
+
+  end
+
+  get 'decisions/home' => 'decisions#home', as: :home
+
+  resource :session, only: [:new, :create, :destroy]
+
+  get 'users' => 'users#index', as: :users
+  get 'users/new' => 'users#new', as: :new_user
+  post 'users' => 'users#create'
+  get 'users/:id' => 'users#show', as: :user
+  get 'users/:id/edit' => 'users#edit', as: :edit_user
+  put 'users/:id' => 'users#update'
+  patch 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
+
+
+  get 'criteriaforanswer' => 'criteriaforanswer#index'
+
+
+end
