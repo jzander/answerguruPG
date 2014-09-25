4d520c9d9faaa630f2a000e05186afd34c867165
jzander <jeremy.zander@gmail.com>
2014-09-24 17:49:38 -0700

    updated create criteria for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -9,7 +9,9 @@ class Answer < ActiveRecord::Base
   accepts_nested_attributes_for :criteria_answers
 
   def make_criteria_answers
+    if !CriteriaAnswer.where(answer_id:self.id).exists?
     # Put all the criteria_answers in place for this new answer.  One per existing criteria.
-    ActiveRecord::Base.connection.execute("INSERT INTO criteria_answers (criterion_id, answer_id) (SELECT id, #{self.id} FROM criteria WHERE decision_id=#{self.decision_id})")
+    ActiveRecord::Base.connection.execute("INSERT INTO criteria_answers (criterion_id, answer_id) (SELECT id, #{self.id} FROM criteria WHERE decision_id=#{self.decision_id}) ")
+    end
   end
 end
